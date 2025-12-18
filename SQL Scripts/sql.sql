c977f8ec314aeedafa891235a070bcf2025cae2a
harshitha-ts14 <harshithats003@gmail.com>
2025-12-18 12:47:51 +0530

    Add files via upload

---
+++ SQL Scripts/sql.sql
@@ -0,0 +1,10 @@
+use organisation;
+create table employees(Emp_id int primary key,Name VARCHAR(50),Department VARCHAR(50),Sales INT);
+insert into employees values(101,'john','sales',50000);
+insert into employees values(102,'priya','marketing',60000);
+insert into employees values(103,'amit','sales',45000);
+insert into employees values(104,'sara','hr',30000);
+insert into employees values(105,'rahul','marketing',70000);
+select * from employees;
+create database organisation;
+
